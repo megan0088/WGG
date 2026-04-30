68599809c4077d9fc0d4498cda4936351320b548
ega <eganeue@gmail.com>
2026-04-30 11:05:04 +0700

    Initial Commit

---
+++ WGG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WGG
+//
+//  Created by Muhamad Ega Nugraha on 30/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WGG/WGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  WGGApp.swift
+//  WGG
+//
+//  Created by Muhamad Ega Nugraha on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
